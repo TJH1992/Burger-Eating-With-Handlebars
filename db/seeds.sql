fec6af77d8a02307ae3a015693b626ea2af4d0ab
TJ Hutchinson <tjhutch92@gmail.com>
2021-01-10 14:07:01 -0800

    Commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Ranch Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gauc Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
