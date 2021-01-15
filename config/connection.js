f8d6f5c98a8ac9b6d92e9928150ecee53378939f
TJ Hutchinson <tjhutch92@gmail.com>
2021-01-14 22:01:33 -0800

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Cs822021623_",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = myql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Cs822021623_',
+    database: 'burgers_db'
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) 
 VALUES ('Bacon Ranch Burger', false), ('Gauc Burger', false), ('Turkey Burger', false), ('Classic Burger', false);
+
+SELECT * FROM burgers;
